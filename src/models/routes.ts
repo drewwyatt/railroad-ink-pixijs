af199ac479ecbd4274c738daee8578ac3cd59223
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-12-17 23:46:00 -0500

    add route types

--- src/models/routes.ts
+++ src/models/routes.ts
@@ -26,4 +26,38 @@ enum Route {
   Empty,
 }
 
+export const NORMAL_ROUTES = [
+  Route.StraightHighway,
+  Route.CurvedHighway,
+  Route.TJunctionHighway,
+  Route.StraightRailway,
+  Route.TJunctionRailway,
+  Route.CurvedRailway,
+  Route.Overpass,
+  Route.StraightStation,
+  Route.CurvedStation,
+] as const
+export type NormalRoute = typeof NORMAL_ROUTES[number]
+export const isNormalRoute = (route: any): route is NormalRoute =>
+  NORMAL_ROUTES.includes(route)
+
+export const SPECIAL_ROUTES = [
+  Route.THighwayRailwayStation,
+  Route.TRailwayHighwayStation,
+  Route.FourWayHighway,
+  Route.FourWayRailway,
+  Route.CurvedHighwayRailwayStation,
+  Route.StraightHighwayRailwayStation,
+]
+export type SpecialRoute = typeof SPECIAL_ROUTES[number]
+export const isSpecialRoute = (route: any): route is SpecialRoute =>
+  SPECIAL_ROUTES.includes(route)
+
+export const EXIT_ROUTES = [Route.ExitHighway, Route.ExitRailway]
+export type ExitRoute = typeof EXIT_ROUTES[number]
+export const isExitRoute = (route: any): route is ExitRoute => EXIT_ROUTES.includes(route)
+
+export type EmptyRoute = Route.Empty
+export const isEmptyRoute = (route: any): route is Route.Empty => route === Route.Empty
+
 export default Route
